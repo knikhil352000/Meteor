295ac49b3ce05e604b53621a83ef27a3b3d16dbf
knikhil352000 <knikhil352000@gmail.com>
2021-04-01 20:35:59 +0530

    Learning framer motion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion, transform, useAnimation, useElementScroll, useMotionValue, useSpring, useTransform  } from "framer-motion"
+import { useEffect, useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const x = useMotionValue(0)
+  // const xRange = [-200, -100, 100, 200]
+  // const opacityRange = [0, 1, 1, 0]
+  // const opacity = useTransform(x, xRange, opacityRange)
+  // const x = useSpring(0, { stiffness: 300 })
+  // const y = useSpring(x, { damping: 10 })
+  // useEffect(() => {
+  //   console.log(x.getVelocity());
+  // }, [x])
+  // return (
+  //     <motion.div className="app"
+  //       style={{ x }}
+  //     />
+  // );
+  const x = useMotionValue(0)
+
+  useEffect(() => {
+    const controls = animate(x, 100, {
+      type: "spring",
+      stiffness: 2000,
+      onComplete: v => {}
+    })
+  })
+  
+
+  return <motion.div className='app' animate={controls} />
 }
 
 export default App;
