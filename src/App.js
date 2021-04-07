292bc6c98238d5125eae593d40f6dfa715d52f7e
knikhil352000 <knikhil352000@gmail.com>
2021-04-07 23:36:07 +0530

    Added MCQ option

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import './App.css';
 import Check from './Check';
 import Meteor from './Meteor';
 const App = () => {
+  const question = [
+    {
+        type: "MCQ",
+        q: "The molecule used by cells to store genetic information is _____",
+        a: "a",
+        optionA: "DNA",
+        optionB: "Nucleus",
+        optionC: "Cell",
+        optionD: "Organelle",
+    },
+    {
+        type: "Fill_In_The_Blanks",
+        q: "The _____ is surrounded by a membrane, contains the cell's DNA, and acts as the control center",
+        a: "nucleus"
+    },
+    {
+        type: "Fill_In_The_Blanks",
+        q: "Small structure within a cell that performs a specific function is the ______",
+        a: "organelle"
+    }
+  ];
   const [input, setInput] = useState("");
   const [answer, setAnswer] = useState('');
   const [check, setCheck] = useState("non");
+  const [quiz, setQuiz] = useState(question[0]);
   const submitAnswer = (e) => {
     e.preventDefault();
     if(input.toLowerCase() === answer.toLowerCase()) {
@@ -20,11 +42,30 @@ const App = () => {
   }
   return (
     <div className="app">
-      <Meteor setAnswer={setAnswer}/>
+      <Meteor setAnswer={setAnswer} setQuiz={setQuiz} question={question} quiz={quiz}/>
       <div className='app__image'>
       </div>
       <form className="app__form" onSubmit={submitAnswer}>
-        <input type="text" autoFocus onChange={(e) => setInput(e.target.value)} value={input}/>
+        {
+          quiz.type !== "MCQ" ? (
+              <input type="text" autoFocus onChange={(e) => setInput(e.target.value)} value={input}/>
+            ) : (
+              <div className="app__mcq">
+                <div className="app__options">
+                  <h3>{quiz.optionA}</h3>
+                </div>
+                <div className="app__options">
+                  <h3>{quiz.optionB}</h3>
+                </div>
+                <div className="app__options">
+                  <h3>{quiz.optionC}</h3>
+                </div>
+                <div className="app__options">
+                  <h3>{quiz.optionD}</h3>
+                </div>
+            </div>
+            )
+        }
       </form>
       {
         (check !== "non") && <Check check={check}/>
--- src/Meteor.js
+++ src/Meteor.js
@@ -1,27 +1,12 @@
 import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import './Meteor.css'
-const Meteor = ({setAnswer}) => {
+const Meteor = ({setAnswer, setQuiz, question, quiz}) => {
     const windowHeight = window.innerHeight;
     const windowWidth = window.innerWidth;
-    const question = [
-        {
-            q: "The molecule used by cells to store genetic information is _____",
-            a: "DNA"
-        },
-        {
-            q: "The _____ is surrounded by a membrane, contains the cell's DNA, and acts as the control center",
-            a: "nucleus"
-        },
-        {
-            q: "Small structure within a cell that performs a specific function is the ______",
-            a: "organelle"
-        }
-    ];
     const [index, setIndex] = useState(0)
-    const [quiz, setQuiz] = useState(question[0]);
     const xValue = [-windowWidth / 3, -windowWidth / 4, -windowWidth / 5, windowWidth / 3, windowWidth / 4, windowWidth / 5, 0];
-    const [duration, setDuration] = useState(0)
+    const [duration, setDuration] = useState(0);
     useEffect(() => {
         const myTimer = setInterval(() => {
             setIndex((prev) => prev + 1);
